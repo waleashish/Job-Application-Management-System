586db8b9113c70e73d22a5a0ca7eecfb9345e883
waleashish <ashishwale747@gmail.com>
2024-06-24 13:49:50 -0700

    Spring Boot application setup

---
+++ server/src/main/java/com/bje/jamapp/JamappApplication.java
@@ -0,0 +1,13 @@
+package com.bje.jamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamappApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/bje/jamapp/JamappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bje.jamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
